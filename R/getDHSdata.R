539833a5b08defd8994272c2fb283a98012cd8f2
Richard Li <lizehang@gmail.com>
2023-07-03 09:24:50 -0700

    initial commit

---
+++ R/getDHSdata.R
@@ -0,0 +1,23 @@
+#' Download DHS data
+#' 
+#' This function downloads all relevant DHS data for a particular country and survey.
+#'
+#' @param country add description to country argument here.
+#' @param survey add description to survey argument here.
+#' 
+#' @return This function returns a list of datasets containing:
+#' \itemize{
+#'   \item IRdata individual file
+#'   \item PRdata 
+#'   \item KRdata 
+#' }
+#' @importFrom rdhs get_datasets
+#' @author Qianyu Dong
+#' @examples
+#' \dontrun{
+#' }
+#' 
+#' @export
+getDHSdata <- function(country, survey) {
+
+}
