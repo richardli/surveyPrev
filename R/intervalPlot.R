6bb8f5f07cd0ba7dcf96e8f9a0e0476ba03c9178
qianyu313 <qianyu313@gmail.com>
2023-08-03 00:39:19 -0500

    add intervalPlot and update scatterPlot

---
+++ R/intervalPlot.R
@@ -0,0 +1,53 @@
+#' Get scatter plot for any two model results
+#'
+#' This function return scatter plot at admin 1 level for any two model results
+#'
+#' @param res  model result  using surveyPrev
+#' @param line1 yintercept
+#' @return This function returns the dataset that contain district name and population for given  tiff files and polygons of admin level
+#'   \item {plot
+#' }
+#' @import ggplot2
+#' @author Qianyu Dong
+#' @examples
+#' \dontrun{
+#' }
+#'
+#' @export
+
+intervalPlot<-function(res){
+
+  data<-res[[1]]
+  linedata<-res[[2]]
+  line2<-res[[3]]$value
+  # con_interval <- data.frame(x = res[[3]]$value, upper = res[[3]]$quant975, lower = res[[3]]$quant025)
+  plot_fun <- function(dat) {
+    line1= linedata[unique(dat$admin1.name),"value"]
+    # line2data<-data.frame(
+    #   xx=dat$admin2.name,
+    #   yymin=rep(res[[3]]$quant025,length(dat$admin1.name)),
+    #   yymax=rep(res[[3]]$quant975,length(dat$admin1.name)) )
+
+    ggplot(dat, aes(x = admin2.name, y = value)) +
+        geom_point() +
+        geom_errorbar(aes(ymin = quant025 , ymax = quant975,color = type), alpha = 0.5) +
+        scale_color_manual(values = c("red", "blue", "darkgreen"))+
+        geom_hline(  aes(yintercept =line1,linetype = "dashed"),color="grey30",size = 1) +
+        geom_hline( aes(yintercept =line2, linetype = "solid"),color="grey30",size = 1) +
+       # geom_ribbon(data = line2data, aes(x =xx, ymin = yymin, ymax = yymax), fill = "grey70") +
+      labs(title = unique(dat$admin1.name))+
+      scale_linetype_manual(values = c("dashed", "solid"),
+                            labels = c("admin1","national")) +
+    # theme(legend.position = "bottom")+
+      theme(axis.text.x = element_text(angle = 45))
+  }
+
+  plots <- lapply(unique(data$admin1.name), function(x) {
+    data <- data %>% filter(admin1.name == x)
+    plot_fun(data)
+  })
+
+  return(plots)
+
+}
+
--- R/scatterPlot.R
+++ R/scatterPlot.R
@@ -1,7 +1,8 @@
 #' Get scatter plot for any two model results
 #'
 #' This function return scatter plot at admin 1 level for any two model results
-#'
+#' @param admin admin level of the plot
+#' @param scatter "estimate" or "se"
 #' @param res1  model result 1 using surveyPrev
 #' @param nameres1 name of model 1
 #' @param res2  model result 2 using surveyPrev
@@ -18,8 +19,9 @@
 #'
 #' @export
 
-scatterPlot<-function(scatter,res1,nameres1,res2,nameres2){
+scatterPlot<-function(admin,scatter,res1,nameres1,res2,nameres2){
 
+if(admin==1){
   if(scatter=="estimate"){
     if(nameres1=="dir.admin1"){
       res1<-res1[[1]]
@@ -150,10 +152,6 @@ scatterPlot<-function(scatter,res1,nameres1,res2,nameres2){
 
   }
 
-
-
-
-
   df<- merge(x = res1, y = res2, by="admin1.name" )
   # print(df)
 
@@ -165,6 +163,113 @@ scatterPlot<-function(scatter,res1,nameres1,res2,nameres2){
     xlab(paste0(xandy[1]))+
     ylab(paste0(xandy[2]))+
   xlim(lim) + ylim(lim)
+
+
+
+}else if(admin==2){
+
+  if(scatter=="estimate"){
+
+
+    if(nameres1=="dir.admin2"){
+      res1<-res1[[1]]
+      name1<- 'direct.est'
+      colnames(res1)[colnames(res1) == 'value'] <-name1
+    }else if(nameres1=="smth.est2"){
+      res1<-res1[[2]]
+      name1<- 'smth.dir.est'
+      colnames(res1)[colnames(res1) == 'value'] <- name1
+     }else if(nameres1=="T2"){
+      res1<-res1[[1]]
+      res1<- res1[which(res1$type =="aggregated"),]
+      name1<- 'strata.est'
+      colnames(res1)[colnames(res1) == 'value'] <- name1
+    }else if(nameres1=="F2"){
+      res1<-res1[[1]]
+      name1<-'cluster.est'
+      colnames(res1)[colnames(res1) == 'value'] <- name1
+    }
+
+      if(nameres2=="dir.admin2"){
+        res2<-res2[[1]]
+        name2<- 'direct.est'
+        colnames(res2)[colnames(res2) == 'value'] <-name2
+      }else if(nameres2=="smth.est2"){
+        res2<-res2[[1]]
+        name2<- 'smth.dir.est'
+        colnames(res1)[colnames(res1) == 'value'] <- name2
+      }else if(nameres2=="T2"){
+        res2<-res2[[1]]
+        res2<-res2[which(res2$type=="aggregated"),]
+        name2<- 'strata.est'
+        colnames(res2)[colnames(res2) == 'value'] <- name2
+      }else if(nameres2=="F2"){
+        res2<-res2[[1]]
+        name2<-'cluster.est'
+        colnames(res2)[colnames(res2) == 'value'] <- name2
+      }
+
+
+
+
+    xandy<-c(name1, name2)
+    # print(xandy)
+
+  }else if (scatter=="se"){
+
+    if(nameres1=="dir.admin2"){
+      res1<-res1[[1]]
+      name1<- 'direct.sd'
+      colnames(res1)[colnames(res1) == 'se'] <-name1
+    }else if(nameres1=="T2"){
+      res1<-res1[[1]]
+      res1<-res1[which(res1$type=="aggregated"),]
+      name1<- 'strata.sd'
+      colnames(res1)[colnames(res1) == 'sd'] <- name1
+    }else if(nameres1=="F2"){
+      res1<-res1[[1]]
+      name1<-'cluster.sd'
+      colnames(res1)[colnames(res1) == 'sd'] <- name1
+    }
+    # } else if(nameres1=="dir.admin2"){
+    #   res1<-res1[[2]]
+    #   colnames(res1)[colnames(res1) == 'weighted_avg'] <- 'agg.dir.est'
+
+    if(nameres2=="dir.admin2"){
+      res2<-res2[[1]]
+      name2<- 'direct.sd'
+      colnames(res2)[colnames(res2) == 'se'] <-name2
+    }else if(nameres2=="T2"){
+      res2<-res2[[1]]
+      res2<-res2[which(res2$type=="aggregated"),]
+      name2<- 'strata.sd'
+      colnames(res2)[colnames(res2) == 'sd'] <- name2
+    }else if(nameres2=="F2"){
+      res2<-res2[[1]]
+      name2<-'cluster.sd'
+      colnames(res2)[colnames(res2) == 'sd'] <- name2
+    }
+
+    xandy<-c(name1, name2)
+    # xandy<-c(colnames(res1)[length(colnames(res1))], colnames(res2)[length(colnames(res2))])
+
+  }
+
+  df<- merge(x = res1, y = res2, by="admin2.name" )
+  # print(df)
+
+  lim <- range(c(df[,c(xandy[1])], df[,c(xandy[2])]), na.rm = TRUE)
+  ggplot(df, aes(x=df[,c(xandy[1])],y=df[,c(xandy[2])])) +
+    geom_point(alpha = 0.5,aes())+
+    geom_abline(slope = 1, intercept = 0, linetype = "dashed")+
+    labs(title = paste0(scatter, ":", xandy[1]," ", "versus", " " ,xandy[2]))+
+    xlab(paste0(xandy[1]))+
+    ylab(paste0(xandy[2]))+
+    xlim(lim) + ylim(lim)
+
+
+}
+
 
 }
 
