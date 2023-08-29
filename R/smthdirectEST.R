53161c5e2f9b2ba0718d18a3e23c8c2ae9e085e5
Richard Li <lizehang@gmail.com>
2023-08-29 11:09:06 -0700

    Important commit: some todo items here. Changed function name to be consistent with clusterModel()

--- R/smthdirectEST.R
+++ R/smthdirectEST.R
@@ -4,11 +4,11 @@
 #'
 #' @param data  dataframe that contains the indicator of interests
 #' @param cluster.info dataframe that contains admin 1 and admin 2 information and coordinates for each cluster.
-#' @param admininfo dataframe that contains population and urban/rural proportion at specific admin level
+#' @param admin.info dataframe that contains population and urban/rural proportion at specific admin level
 #' @param admin admin level for the model
 #'
 #' @return This function returns the dataset that contain district name and population for given  tiff files and polygons of admin level,
-#'   \item { smthdirectEST.result
+#'   \item { 
 #' }
 #' @import dplyr
 #' @importFrom survey svydesign svyby
@@ -21,11 +21,12 @@
 #' @export
 
 
-smthdirectEST <- function(data, cluster.info, admininfo, admin, Amat ){
+fhModel <- function(data, cluster.info, admin.info, admin ){
   if(sum(is.na(data$value))>0){
     data <- data[rowSums(is.na(data)) == 0, ]
     message("Removing NAs in indicator response")
   }
+  Amat <- admin.info$admin.mat
 
   if(admin==2){
 
@@ -52,9 +53,12 @@ smthdirectEST <- function(data, cluster.info, admininfo, admin, Amat ){
 
 
 
-
+    ##
+    ## TODO: also need to deal with duplicated admin2 names
+    ##       the below codes computes only overall proportion? The group_by call needs to be before mutate?
+    ##
     #aggregate results
-    admin1_agg<- left_join(admin2_res,admininfo,by="admin2.name")%>%
+    admin1_agg<- left_join(admin2_res,admin.info$admin.info,by="admin2.name")%>%
       mutate(prop=population/sum(population))%>%
       group_by(admin1.name) %>%
       summarise(weighted_avg = weighted.mean(value, prop))
@@ -62,7 +66,7 @@ smthdirectEST <- function(data, cluster.info, admininfo, admin, Amat ){
 
 
 
-    nation_agg<- left_join(admin2_res,admininfo,by="admin2.name")%>%
+    nation_agg<- left_join(admin2_res,admin.info$admin.info,by="admin2.name")%>%
       mutate(prop=population/sum(population))%>%
       summarise(weighted_avg = weighted.mean(value, prop))%>%
       mutate(weighted_avg = sprintf("%.4f", weighted_avg))
@@ -96,7 +100,7 @@ smthdirectEST <- function(data, cluster.info, admininfo, admin, Amat ){
 
 
     # aggregate results
-    nation_agg<- left_join(admin1_res,admininfo,by="admin1.name")%>%
+    nation_agg<- left_join(admin1_res,admin.info$admin.info,by="admin1.name")%>%
       mutate(prop=population/sum(population))%>%
       summarise(weighted_avg = weighted.mean(value, prop))%>%
       mutate(weighted_avg = sprintf("%.4f", weighted_avg))
@@ -104,42 +108,42 @@ smthdirectEST <- function(data, cluster.info, admininfo, admin, Amat ){
     return(list(admin1.res=admin1_res,agg.natl=nation_agg,fit1))
 
     }else if(admin==0){
+    stop("No estimates to smooth at admin0 level.")
+    # data$admin0.name="Zambia"
+    # modt<- left_join(data,cluster.info$cluster.info,by="cluster")
+    # modt<- modt[!(is.na(modt$LONGNUM)), ]
+    # modt$strata.full <- paste(modt$admin1.name, modt$strata)
 
-    data$admin0.name="Zambia"
-    modt<- left_join(data,cluster.info$cluster.info,by="cluster")
-    modt<- modt[!(is.na(modt$LONGNUM)), ]
-    modt$strata.full <- paste(modt$admin1.name, modt$strata)
+    # # Amat=NULL
 
-    # Amat=NULL
-
-    # clusterVar = "~cluster+householdID"
-    # design <- survey::svydesign(ids = stats::formula(clusterVar),
-    #                             weights = ~weight , data = modt)
-    #
-    # admin0_res <- survey::svyby(formula = ~value, by = ~admin0.name,
-    #                       design = design,
-    #                       survey::svymean,
-    #                       drop.empty.groups = FALSE)
+    # # clusterVar = "~cluster+householdID"
+    # # design <- survey::svydesign(ids = stats::formula(clusterVar),
+    # #                             weights = ~weight , data = modt)
+    # #
+    # # admin0_res <- survey::svyby(formula = ~value, by = ~admin0.name,
+    # #                       design = design,
+    # #                       survey::svymean,
+    # #                       drop.empty.groups = FALSE)
 
 
-    smoothSurvey_res<-smoothSurvey(as.data.frame(modt),
-                                   responseType ="binary",
-                                   responseVar= "value",
-                                   regionVar = "admin0.name",
-                                   clusterVar = "~cluster+householdID",
-                                   weightVar = "weight",
-                                   strataVar = "strata.full",
-                                   Amat =NULL,
-                                   CI = 0.95,
-                                   is.unit.level=FALSE,
-                                   smooth=T)
-    admin0_res<-smoothSurvey_res$smooth
-    colnames(admin0_res)[1:3] <- c("admin0.name","value","var")
-    colnames(admin0_res)[5:6] <- c("quant025","quant975")
+    # smoothSurvey_res<-smoothSurvey(as.data.frame(modt),
+    #                                responseType ="binary",
+    #                                responseVar= "value",
+    #                                regionVar = "admin0.name",
+    #                                clusterVar = "~cluster+householdID",
+    #                                weightVar = "weight",
+    #                                strataVar = "strata.full",
+    #                                Amat =NULL,
+    #                                CI = 0.95,
+    #                                is.unit.level=FALSE,
+    #                                smooth=T)
+    # admin0_res<-smoothSurvey_res$smooth
+    # colnames(admin0_res)[1:3] <- c("admin0.name","value","var")
+    # colnames(admin0_res)[5:6] <- c("quant025","quant975")
 
-    admin0_res$sd<-sqrt(admin0_res$var)
+    # admin0_res$sd<-sqrt(admin0_res$var)
 
-    return(admin0.res=admin0_res)
+    # return(admin0.res=admin0_res)
 
     }
 
