6116c5d43e48e8398d347946ba2a35b81dbd2fa9
Richard Li <lizehang@gmail.com>
2023-08-29 10:49:17 -0700

    some minor updates to aggPopulation (more to be done here: see vignette TODOs)

--- R/aggPopulation.R
+++ R/aggPopulation.R
@@ -2,7 +2,8 @@
 #'
 #' This function aggregate population to particular admin levels
 #'
-#' @param tiff100  spatial point dataframe
+#' @param tiff  spatial raster of population estimates.
+#' @param fact factor to aggregate pixels. Default to be 10, i.e., the population estimates will be saved on 1km by 1km grids if the input is 100m by 100m tiff. Larger values of aggregation factor improves the computation speed, but can introduce more errors when the regions defined by the polygon are small in size.
 #' @param SpatialPolygons spatial polygons dataframe.
 #' @param varname column name of district name in SpatialPolygons file.
 #'
@@ -19,9 +20,13 @@
 #' @export
 
 
-aggPopulation <- function(tiff_100m , SpatialPolygons, varname){
+aggPopulation <- function(tiff, fact = 10, SpatialPolygons, varname){
 
-  pop_aggre <- aggregate(tiff_100m, fact=10,sum)
+  if(fact > 1){
+    pop_aggre <- aggregate(tiff, fact = fact, sum)
+  }else{
+    pop_aggre <- tiff
+  }
   pop_aggre.df<- raster::as.data.frame(pop_aggre, xy=TRUE)
   colnames(pop_aggre.df)[3] <- "population"
 
