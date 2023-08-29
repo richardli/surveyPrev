729d18f4b211e039a8a48807edd475f06248563c
Richard Li <lizehang@gmail.com>
2023-08-28 23:11:42 -0700

    add pop data as example dataset

---
+++ R/ZambiaPopWomen.R
@@ -0,0 +1,15 @@
+#' Population estimates for Women of age 15 to 49 in Zambia in 2018. 
+#' 
+#' A list of three objects
+#' \itemize{
+#' \item raster A 100m by 100m raster file for the population estimates for women of age 15 to 49 in Zambia in 2018
+#' \item admin1_urban A data frame specifying the proportion of urban population (as defined by those living in regions designated as urban in the previous census) for Women of age 15 to 49 in Zambia in 2018, in each admin1 region.
+#' \item admin2_urban A data frame specifying the proportion of urban population (as defined by those living in regions designated as urban in the previous census) for Women of age 15 to 49 in Zambia in 2018, in each admin2 region. The corresponding admin1 region name is also included. 
+#' }
+#' The dataset is downloaded from WorldPop (https://hub.worldpop.org/geodata/summary?id=16429) and post processed.
+#' 
+#' @docType data
+#'
+#' @usage data(ZambiaPopWomen)
+#' 
+"ZambiaPopWomen"
