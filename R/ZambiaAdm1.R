178c9faaaf736af68bee1c2173f8e19014d690c6
Richard Li <lizehang@gmail.com>
2023-08-28 17:03:38 -0700

    add Zambia admin maps to data

---
+++ R/ZambiaAdm1.R
@@ -0,0 +1,9 @@
+#' Admin 1 Polygon Map for Zambia. 
+#' 
+#' A SpatialPolygonsDataFrame corresponding to Zambia's admin-1 regions. The dataset is downloaded from GADM (https://gadm.org/data.html) version 4.1.  
+#' 
+#' @docType data
+#'
+#' @usage data(ZambiaAdm1)
+#' 
+"ZambiaAdm1"
---
+++ R/ZambiaAdm2.R
@@ -0,0 +1,9 @@
+#' Admin 2 Polygon Map for Zambia. 
+#' 
+#' A SpatialPolygonsDataFrame corresponding to Zambia's admin-2 regions. The dataset is downloaded from GADM (https://gadm.org/data.html) version 4.1.  
+#' 
+#' @docType data
+#'
+#' @usage data(ZambiaAdm2)
+#' 
+"ZambiaAdm2"
