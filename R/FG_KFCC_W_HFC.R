fce8e30802306da342b94b8b774f8510ef1f4c66
qianyu313 <qianyu313@gmail.com>
2025-04-03 23:57:24 -0700

    add FG_KFCC_W_HFC and FG_PFCC_W_WCC

---
+++ R/FG_KFCC_W_HFC.R
@@ -0,0 +1,53 @@
+#'FG_KFCC_W_HFC
+#'IRdata
+#'Percentage of women who have ever heard of female circumcision
+#'
+#'
+#' @param Rdata  data.frame from survryPrev::getDHSdata
+#'
+#' @return A partially processed data.frame that will be used in  survryPrev::getDHSindicator. The whole function can be used as a parameter in survryPrev::getDHSindicator
+#'
+#' @author Qianyu Dong
+#' @examples
+#' \dontrun{
+#' dhsData <- getDHSdata(country = "Zambia",
+#'                                  indicator = "FP_CUSA_W_MOD",
+#'                                  year = 2018)
+#' data <- getDHSindicator(dhsData, indicator = NULL,
+#'                          FUN = surveyPrev::fp_cruse_mod)
+#' }
+#' @export
+FG_KFCC_W_HFC <- function(Rdata){
+
+    # # weight variable
+    IRdata <- Rdata %>%
+      mutate(wt = v005/1000000)
+
+    # # //Heard of female circumcision
+    # IRdata <- IRdata %>%
+    #   mutate(fg_heard =
+    #            case_when(
+    #              g100==1 | g101==1 ~ 1 ,
+    #              g100==0 & g101==0 ~ 0 )) %>%
+    #   set_value_labels(fg_heard = c("Yes" = 1, "No"=0)) %>%
+    #   set_variable_labels(fg_heard = "Heard of female circumcision")
+    #
+    # QY: 04/03/2025, g101 all NA in HG2018, so make and& to or|
+    # //Heard of female circumcision
+    IRdata <- IRdata %>%
+      mutate(FG_KFCC_W_HFC =
+               case_when(
+                 g100==1 | g101==1 ~ 1 ,
+                 g100==0 | g101==0 ~ 0 )) %>%
+      set_value_labels(FG_KFCC_W_HFC = c("Yes" = 1, "No"=0)) %>%
+      set_variable_labels(FG_KFCC_W_HFC = "Heard of female circumcision")
+
+    colnames(IRdata)[colnames(IRdata) == 'FG_KFCC_W_HFC'] <- "value"
+    return(IRdata)
+
+
+  }
+
+
+
+
---
+++ R/FG_PFCC_W_WCC.R
@@ -0,0 +1,40 @@
+#'FG_PFCC_W_WCC
+#'IRdata
+#'Percentage of women circumcised (women who experienced female genital cutting (FGM))
+#'
+#'
+#' @param Rdata  data.frame from survryPrev::getDHSdata
+#'
+#' @return A partially processed data.frame that will be used in  survryPrev::getDHSindicator. The whole function can be used as a parameter in survryPrev::getDHSindicator
+#'
+#' @author Qianyu Dong
+#' @examples
+#' \dontrun{
+#' dhsData <- getDHSdata(country = "Zambia",
+#'                                  indicator = "FP_CUSA_W_MOD",
+#'                                  year = 2018)
+#' data <- getDHSindicator(dhsData, indicator = NULL,
+#'                          FUN = surveyPrev::fp_cruse_mod)
+#' }
+#' @export
+FG_PFCC_W_WCC <- function(Rdata){
+
+
+  IRdata <- Rdata %>%
+    mutate(FG_PFCC_W_WCC =
+             case_when(
+               g102==1 ~ 1 ,
+               g102==0 | g100==0 | g100==1 ~ 0)) %>%
+    set_value_labels(FG_PFCC_W_WCC = c("Yes" = 1, "No"=0)) %>%
+    set_variable_labels(FG_PFCC_W_WCC = "Circumcised among women age 15-49")
+
+
+  colnames(IRdata)[colnames(IRdata) == 'FG_PFCC_W_WCC'] <- "value"
+  return(IRdata)
+
+
+}
+
+
+
+
