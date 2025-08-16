a1bb8bc3d8bea35291765e91fe29f21cb05d05a4
Richard Li <lizehang@gmail.com>
2025-08-16 14:26:53 -0700

    add missing data documentation

---
+++ R/match_all_result.R
@@ -0,0 +1,9 @@
+#' Indicator matching table. 
+#' 
+#' A data frame with DHS indicator information used to match across different code bases. 
+#' 
+#' @docType data
+#'
+#' @usage data(match_all_result)
+#' 
+"match_all_result"
